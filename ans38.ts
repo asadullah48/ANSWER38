95343d5e0f2dd78d549c86033121eda5ec6f7896
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-05-12 11:10:28 +0500

    Initial commit

---
+++ ans38.ts
@@ -0,0 +1,7 @@
+function describing_cities(city: string, country: string = "United Arab Emirates"){
+console.log(`${city} is in ${country}`)
+};
+describing_cities("Dubai", "United Arab Emirates");
+describing_cities("Karachi", "Pakistan");
+describing_cities("Delhi", "India");
+describing_cities("Madina", "Saudi Arab");
